38f593b5f8e32aff78346dcb0a8e38435292c33e
finbargiusti <finbargiusti@gmail.com>
2022-10-28 20:55:35 +0100

    bad testing

--- js/Screen.ts
+++ js/Screen.ts
@@ -259,6 +259,8 @@ export default class Screen {
       if (realR < 3) {
         return;
       }
+
+      console.log(this.images[d.label]);
       // replace small circles with placeholder, so it is not jarring.
       if (realR < 5 || !this.images[d.label]) {
         ctx.save();
